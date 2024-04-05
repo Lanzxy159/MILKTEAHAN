f58da9f02851e08ed57dca3a00a67561b5586e63
Lanzxy159 <95922499+Lanzxy159@users.noreply.github.com>
2024-04-05 17:36:21 +0800

    Update app.js
    4/5/24 5:36

--- app.js
+++ app.js
@@ -13,39 +13,26 @@ Item_1_Enter.addEventListener('click', function(){
         total_price_quantity: total_price_quantity
     };
     item_orders.push(item_1_object);
-
-
-
-
     const Ordered_Items = document.getElementById('Ordered_Items');
-   
     var cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
     var cardBodyDiv = document.createElement('div');
     cardBodyDiv.classList.add('card-body');
-
     var cardTitle = document.createElement('h5');
     cardTitle.classList.add('card-title');
     cardTitle.textContent = item_1_object.item_1_item;
-
     var cardText = document.createElement('p');
     cardText.classList.add('card-text');
     cardText.textContent = "QTY: "+ item_1_object.Item_1_quantity;
-
     var cardText_total = document.createElement('p');
     cardText_total.classList.add('card-text');
     cardText_total.textContent = "PHP: "+ item_1_object.total_price_quantity;
-
-    
     cardBodyDiv.appendChild(cardTitle);
     cardBodyDiv.appendChild(cardText);
     cardBodyDiv.appendChild(cardText_total);
-
     cardDiv.appendChild(cardBodyDiv);
     Ordered_Items.appendChild(cardDiv);
-
-    
-
+    update_total();
 
 });
 
@@ -56,11 +43,37 @@ Item_2_Enter.addEventListener('click', function(){
     var Item_2_quantity = document.querySelector('#Item_2_quantity').value;
     var total_price_quantity = item_2_price * Item_2_quantity;
     var item_2_object = {
+        item_2_item: 'ROCKY ROAD',
         item_2_price: 49,
+        Item_2_quantity: Item_2_quantity,
         total_price_quantity: total_price_quantity
     };
     item_orders.push(item_2_object);
-    // console.log(item_orders[0].total_price_quantity);
+
+    
+    item_orders.push(item_2_object);
+    const Ordered_Items = document.getElementById('Ordered_Items');
+    var cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    var cardBodyDiv = document.createElement('div');
+    cardBodyDiv.classList.add('card-body');
+    var cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = item_2_object.item_2_item;
+    var cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = "QTY: "+ item_2_object.Item_2_quantity;
+    var cardText_total = document.createElement('p');
+    cardText_total.classList.add('card-text');
+    cardText_total.textContent = "PHP: "+ item_2_object.total_price_quantity;
+    cardBodyDiv.appendChild(cardTitle);
+    cardBodyDiv.appendChild(cardText);
+    cardBodyDiv.appendChild(cardText_total);
+    cardDiv.appendChild(cardBodyDiv);
+    Ordered_Items.appendChild(cardDiv);
+    update_total();
+
+
 });
 
 // Item 3 Value
@@ -70,11 +83,33 @@ Item_3_Enter.addEventListener('click', function(){
     var Item_3_quantity = document.querySelector('#Item_3_quantity').value;
     var total_price_quantity = item_3_price * Item_3_quantity;
     var item_3_object = {
+        item_3_item: 'BUBBLE GUM',
         item_3_price: 39,
+        Item_3_quantity: Item_3_quantity,
         total_price_quantity: total_price_quantity
     };
     item_orders.push(item_3_object);
-    // console.log(item_orders[0].total_price_quantity);
+    const Ordered_Items = document.getElementById('Ordered_Items');
+    var cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    var cardBodyDiv = document.createElement('div');
+    cardBodyDiv.classList.add('card-body');
+    var cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = item_3_object.item_3_item;
+    var cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = "QTY: "+ item_3_object.Item_3_quantity;
+    var cardText_total = document.createElement('p');
+    cardText_total.classList.add('card-text');
+    cardText_total.textContent = "PHP: "+ item_3_object.total_price_quantity;
+    cardBodyDiv.appendChild(cardTitle);
+    cardBodyDiv.appendChild(cardText);
+    cardBodyDiv.appendChild(cardText_total);
+    cardDiv.appendChild(cardBodyDiv);
+    Ordered_Items.appendChild(cardDiv);
+    update_total();
+
 });
 
 
@@ -85,11 +120,32 @@ Item_4_Enter.addEventListener('click', function(){
     var Item_4_quantity = document.querySelector('#Item_4_quantity').value;
     var total_price_quantity = item_4_price * Item_4_quantity;
     var item_4_object = {
+        item_4_item: 'COKE',
         item_4_price: 29,
+        Item_4_quantity: Item_4_quantity,
         total_price_quantity: total_price_quantity
     };
     item_orders.push(item_4_object);
-    console.log(item_orders[0].total_price_quantity);
+    const Ordered_Items = document.getElementById('Ordered_Items');
+    var cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    var cardBodyDiv = document.createElement('div');
+    cardBodyDiv.classList.add('card-body');
+    var cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = item_4_object.item_4_item;
+    var cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    cardText.textContent = "QTY: "+ item_4_object.Item_4_quantity;
+    var cardText_total = document.createElement('p');
+    cardText_total.classList.add('card-text');
+    cardText_total.textContent = "PHP: "+ item_4_object.total_price_quantity;
+    cardBodyDiv.appendChild(cardTitle);
+    cardBodyDiv.appendChild(cardText);
+    cardBodyDiv.appendChild(cardText_total);
+    cardDiv.appendChild(cardBodyDiv);
+    Ordered_Items.appendChild(cardDiv);
+    update_total();
 });
 
 
@@ -97,6 +153,25 @@ Item_4_Enter.addEventListener('click', function(){
 
 
 //ORDERS//
+var total_orders = document.getElementById('total_amount');
 
+function update_total() {
+    var totalOrderPrice = 0;
+    for (var i = 0; i < item_orders.length; i++) {
+        totalOrderPrice += item_orders[i].total_price_quantity;
+    }
+    total_orders.textContent = totalOrderPrice;
+}
 
+var payment = document.getElementById('payment_button');
+payment.addEventListener('click', function() {
+    var payment_price = parseFloat(document.getElementById('payment_price').value);
 
+    if (payment_price < total_orders.textContent) {
+        alert("MAGBAYAD KA NAMAN NG TAMA TOL!");
+        
+    } else {
+        var change = payment_price - parseFloat(total_orders.textContent);
+        alert("SALAMAT SA BAYAD Your change is PHP: " + change.toFixed(2));
+    }
+});
